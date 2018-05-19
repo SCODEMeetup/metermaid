e678ef84829a1f62f25ee6e2e2165a86fdf4a29e
Bill Schwanitz <bilsch@bilsch.org>
2018-05-19 13:27:34 -0400

    fix inverted lat/lon in the mapping code

--- src/csv_scratch.py
+++ src/csv_scratch.py
@@ -17,8 +17,8 @@ data = pd.read_csv(csv_file,
 key = 'find_your_api_key'
 
 # Center on the grid
-map_options = GMapOptions(lat=data.get('X').mean(),
-                          lng=data.get('Y').mean(),
+map_options = GMapOptions(lat=data.get('Y').mean(),
+                          lng=data.get('X').mean(),
                           map_type="roadmap",
                           zoom=11)
 
@@ -26,8 +26,8 @@ p = gmap(google_api_key=key, map_options=map_options, title="Columbus?")
 
 source = ColumnDataSource(
     dict(
-        lat = data.get("X"),
-        lon = data.get("Y")
+        lat = data.get("Y"),
+        lon = data.get("X")
     )
 )
 
